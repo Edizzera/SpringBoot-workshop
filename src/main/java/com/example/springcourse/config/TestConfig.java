e48a65c5e56ffa35a0642ab0302c56809f9a1854
Edizzera <moraisedi.dev@gmail.com>
2021-11-18 17:52:26 -0300

    Payment One-to-One association

--- src/main/java/com/example/springcourse/config/TestConfig.java
+++ src/main/java/com/example/springcourse/config/TestConfig.java
@@ -77,8 +77,8 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
-
-
-
+        Payment pay1 = new Payment(null,Instant.parse("2021-06-20T21:53:07Z"),o1);
+        o1.setPayment(pay1);
+        oderRepository.save(o1);
     }
 }
